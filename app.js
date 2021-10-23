cba440fbdce32538efdf4670281da9bd897dee0d
jjw2763 <jinjooisme@gmail.com>
2021-10-23 20:41:22 +0900

    5. 라우터 화면처리기 만들기

--- app.js
+++ app.js
@@ -9,40 +9,56 @@ function getData(url) {
   ajax.open('GET', url, false);
   ajax.send();
 
-  return JSON.parse(ajax.response);
+  return JSON.parse(ajax.response); //json을 객체로 변환
 }
 
-const newsFeed = getData(NEWS_URL); //json을 객체로 변환
-const ul = document.createElement('ul');
-
-window.addEventListener('hashchange', function() {
-  const id = location.hash.substr(1);
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL); 
+  const newsList = [];
   
-  const newsContent = getData(CONTENT_URL.replace('@id', id));
-  const title = document.createElement('h1');
+  newsList.push('<ul>');
 
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
+  for(let i = 0; i < 10; i++) {
+    newsList.push(`
+      <li>
+        <a href="#${newsFeed[i].id}">
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+      </li>
+    `);
+  }
 
-for(let i = 0; i < 10; i++) {
-  const div = document.createElement('div');
-  const li = document.createElement('li');
-  const a = document.createElement('a');
+  newsList.push('</ul>');
 
-  div.innerHTML = `
-    <li>
-      <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-      </a>
-    </li>
-  `;
-
-  ul.appendChild(div.firstElementChild);
+  container.innerHTML = newsList.join('');
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+  const id = location.hash.substr(1);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
+  
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+      <a href="#">목록으로</a>
+    </div>
+  `;
+}
+
+function router() {
+  const routePath = location.hash;
+
+  if(routePath === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
 
 
 
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -127,30 +127,40 @@ var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 function getData(url) {
   ajax.open('GET', url, false);
   ajax.send();
-  return JSON.parse(ajax.response);
+  return JSON.parse(ajax.response); //json을 객체로 변환
 }
 
-var newsFeed = getData(NEWS_URL); //json을 객체로 변환
+function newsFeed() {
+  var newsFeed = getData(NEWS_URL);
+  var newsList = [];
+  newsList.push('<ul>');
 
-var ul = document.createElement('ul');
-window.addEventListener('hashchange', function () {
+  for (var i = 0; i < 10; i++) {
+    newsList.push("\n      <li>\n        <a href=\"#".concat(newsFeed[i].id, "\">\n          ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n        </a>\n      </li>\n    "));
+  }
+
+  newsList.push('</ul>');
+  container.innerHTML = newsList.join('');
+}
+
+function newsDetail() {
   var id = location.hash.substr(1);
   var newsContent = getData(CONTENT_URL.replace('@id', id));
-  var title = document.createElement('h1');
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
-
-for (var i = 0; i < 10; i++) {
-  var div = document.createElement('div');
-  var li = document.createElement('li');
-  var a = document.createElement('a');
-  div.innerHTML = "\n    <li>\n      <a href=\"#".concat(newsFeed[i].id, "\">\n        ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n      </a>\n    </li>\n  ");
-  ul.appendChild(div.firstElementChild);
+  container.innerHTML = "\n    <h1>".concat(newsContent.title, "</h1>\n\n    <div>\n      <a href=\"#\">\uBAA9\uB85D\uC73C\uB85C</a>\n    </div>\n  ");
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function router() {
+  var routePath = location.hash;
+
+  if (routePath === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router);
+router();
 },{}],"C:/Users/USER/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -179,7 +189,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57256" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50034" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
